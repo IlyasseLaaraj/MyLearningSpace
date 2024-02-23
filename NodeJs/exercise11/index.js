1e398f24865fe1228aa6bce3da6ab3a419333799
IlyasseLaaraj <laarajilyasse001@gmail.com>
2024-02-23 18:46:44 +0100

    added exercise 11

---
+++ NodeJs/exercise11/index.js
@@ -0,0 +1,35 @@
+const { EventEmitter } = require("node:events");
+
+const createNewsFeed = () => {
+  const emitter = new EventEmitter();
+
+  emitter.on("newsEvent", (newsEvent) => {
+    console.log(newsEvent);
+  });
+
+  setInterval(() => {
+    emitter.emit("newsEvent", "News: A thing happened in a place.");
+  }, 1000);
+
+  emitter.on("breakingNews", (breakingNews) => {
+    console.log(breakingNews);
+  });
+
+  setInterval(() => {
+    emitter.emit("breakingNews", "Breaking news! A BIG thing happened.");
+  }, 4000);
+
+  emitter.on("error", (error) => {
+    console.log(error);
+  });
+
+  setTimeout(() => {
+    emitter.emit("error", new Error("News feed connection error"));
+  }, 5000);
+
+  return emitter;
+};
+
+const newsFeed = createNewsFeed();
+
+console.log(newsFeed);
