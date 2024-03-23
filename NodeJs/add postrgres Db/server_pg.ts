24f58b10f33943b673291f00453e338eace88d89
IlyasseLaaraj <laarajilyasse001@gmail.com>
2024-03-24 00:44:36 +0100

    done

---
+++ NodeJs/add postrgres Db/server_pg.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+
+import express from 'express';
+import "express-async-errors";
+import morgan from "morgan";
+import {getAll, getOneById, create, updateById, deleteById } from "./planets_pg.js"
+
+const app = express()
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+app.get('/api/planets', getAll);
+
+app.get('/api/planets/:id', getOneById);
+
+app.post('/api/planets', create)
+
+app.put('/api/planets/:id', updateById);
+
+app.delete('/api/planets/:id', deleteById);
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`Example app listening on port ${process.env.SERVER_PORT}`)
+});
